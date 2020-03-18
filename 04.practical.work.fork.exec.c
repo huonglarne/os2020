9bd9326860dc476fc80d671a2f9988fe76219666
huonglarne <nguyenlanhuong.vietnam@gmail.com>
2020-03-18 19:31:05 +0700

    Reupload Labwork 4

--- 04.practical.work.fork.exec.c
+++ 04.practical.work.fork.exec.c
@@ -5,23 +5,21 @@
 int main(){
 
 	int n1=fork();
-
 	if (n1==0){
+		printf("Child after 1st fork\n");
 
 		int n2=fork();
-
 		if (n2==0){
-			
+			printf("Child after 2nd fork\n");	
 			char *args[] = {"ps", "-ef", NULL};
 			execvp("ps", args);
-			
+		} else {
+			char *args[] ={"free", "-h", NULL};
+			execvp("free", args);
 		}
+	} else printf("Parent of 1st fork\n");
 
-		char *args[] ={"free", "-h", NULL};
-		execvp("free", args);
-
-	}
-	printf("done");
+	printf("done\n");
 	return 0;
 }
 	
